c2a0e9b34a62cb9700b86c4b0f42838c6bd86b96
kevin kelly <cif.web.tech@gmail.com>
2012-08-31 07:22:34 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mycue::Application.config.secret_token = '7263a5e458fd29426f9bdf5d1f88abb6450e95e2773d77ce00d97fa0b5a9d1c278879346fe342466d87a6cf14ebf77e5e7a42a51ba0e837c4c51eb680f4a66bf'
