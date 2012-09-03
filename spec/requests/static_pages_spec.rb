1879102f81b6c44594e3eabd52ad54a99eb908b3
kevin kelly <cif.web.tech@gmail.com>
2012-09-03 09:42:21 -0500

    Change CSS and streamlined rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,22 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-	let (:base_title) { "myCue" }
+  subject { page }
+
   describe "home page (root)" do
+    before { visit root_path }
 
-    it "should only have base title" do
-    	visit root_path
-    	page.should have_selector('title', text: "#{base_title}")
-    end
-
-    it "should not have 'Home' in the root path" do
-    	visit root_path
-    	page.should_not have_selector('title', text: "| Home")
-    end
-
-    it "should have 'myCue' in h1" do
-    	visit root_path
-    	page.should have_selector('h1', text: "myCue")
-    end
-
-    it "should have subtitle in h2" do 
-    	visit root_path
-    	page.should have_selector('h2', text: "Learn your lines with myCue!")
-    end
+    it { should have_selector('title', text: full_title('')) }
+    it { should_not have_selector('title', text: "| Home") }
+    it { should have_selector('h1', text: "myCue") }
+    it { should have_selector('h2', text: "Learn your lines with myCue!") }
     
   end
 
   describe "help page" do
+    before { visit help_path }
 
-  	it "should have 'Help' in the title" do
-  		visit help_path
-  		page.should have_selector('title', text: "#{base_title} | Help")
-  	end
-
-  	it "should be have /about as it's path" do
-  		visit help_path
-  		page.should have_selector('title', text: "#{base_title} | Help")
-  	end
-
-  	it "should have 'Help' in the h1 header" do
-  	  visit help_path
-  	  page.should have_selector('h1', text: "Help")
-  	end
+  	it { should have_selector('title', text: full_title('Help')) }
+  	it { should have_selector('h1', text: "Help") }
   end
 end
